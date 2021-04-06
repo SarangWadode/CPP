020981b2f561961bd99aac6613d4437aaa87b07c
Sarang Wadode <sarangwadode@gmail.com>
2021-04-06 14:40:25 +0530

    Day-1 of cpp revision

---
+++ Day-1/hello.cpp
@@ -0,0 +1,14 @@
+// Hello this is my last time first cpp code.
+
+// Importing library
+#include <iostream> // Precompiled code.
+
+using namespace std;
+
+int main(void)
+{
+
+    //Hello from sarang
+    cout << "Hello from Sarang, How are you?" << endl;
+    return 0; //this tells os that program execution is sucessful.
+}
